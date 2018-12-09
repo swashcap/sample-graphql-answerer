bce6b84adc16d8510d76e70b93aa2eea850c8496
Cory Reed <swashcap@gmail.com>
2018-12-09 12:00:08 -0800

    fix: mock CSS modules for tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
   preset: 'ts-jest',
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
--- src/client/components/__tests__/Label.tsx
+++ src/client/components/__tests__/Label.tsx
@@ -6,8 +6,8 @@ import Label from '../Label';
 test('renders', () => {
   const testRenderer = TestRenderer.create(<Label htmlFor="id">Whoa</Label>);
 
-  expect(testRenderer.root.findByType('label').props).toEqual({
-    children: 'Whoa',
-    htmlFor: 'id',
-  });
+  const { props } = testRenderer.root.findByType('label');
+
+  expect(props).toHaveProperty('children', 'Whoa');
+  expect(props).toHaveProperty('htmlFor', 'id');
 });
